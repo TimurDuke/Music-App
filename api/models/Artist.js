977ae101345c75851a33d3f7fa67f65140740d8a
Timur Kim <kimtimur01@gmail.com>
2022-09-08 20:48:35 +0600

    Created an artist model. (Api)

---
+++ api/models/Artist.js
@@ -0,0 +1,20 @@
+const {Schema, model} = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const ArtistSchema = new Schema({
+   name: {
+       type: String,
+       maxLength: 255,
+       minLength: 1,
+       required: true,
+       unique: true,
+   },
+    information: String,
+    image: String,
+});
+
+ArtistSchema.plugin(uniqueValidator, {message: "An artist with that name already exists."});
+
+const Artist = model("Artist", ArtistSchema);
+
+module.exports = Artist;
