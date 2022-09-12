380e8fb51dd7fbfdba00b92d7477e2cc9b6a6f7a
Timur Kim <kimtimur01@gmail.com>
2022-09-13 00:42:59 +0600

    Created a model for the history of listening to tracks. (Api)

---
+++ api/models/TrackHistory.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const { Schema, model } = mongoose;
+
+const TrackHistorySchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    track: {
+        type: Schema.Types.ObjectId,
+        ref: "Track",
+        required: true,
+    },
+    datetime: {
+        type: Date,
+        required: true,
+    }
+});
+
+const TrackHistory = model("TrackHistory", TrackHistorySchema);
+
+module.exports = TrackHistory;
