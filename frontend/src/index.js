3f95042538b1ad6d456442bdd5219bc7f57b8e4f
Timur Kim <kimtimur01@gmail.com>
2022-09-28 15:22:43 +0600

    #87 Stored the user's state in localStorage so that when the page is refreshed, the user remains logged in.(Frontend)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,8 +9,35 @@ import history from "./history";
 import musicReducer from "./store/reducers/musicReducer";
 import usersReducer from "./store/reducers/usersReducer";
 import tracksHistoryReducer from "./store/reducers/tracksHistoryReducer";
+
 import App from './App';
 import './index.css';
+
+const saveToLocalStorage = state => {
+    try {
+        const serializedState = JSON.stringify(state);
+
+        localStorage.setItem("state", serializedState);
+    } catch (e) {
+        console.error(e);
+    }
+};
+
+const loadFromLocalStorage = () => {
+    try {
+        const serializedState = localStorage.getItem('state');
+
+        if (serializedState === null) {
+            return undefined;
+        }
+
+        return JSON.parse(serializedState);
+    } catch (e) {
+        console.error(e);
+
+        return undefined;
+    }
+};
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -20,10 +47,18 @@ const rootReducer = combineReducers({
     tracksHistory: tracksHistoryReducer,
 });
 
+const persistedState = loadFromLocalStorage();
+
 const store = createStore(
     rootReducer,
+    persistedState,
     composeEnhancers(applyMiddleware(thunk))
 );
+store.subscribe(() => {
+    saveToLocalStorage({
+        users: store.getState().users,
+    });
+});
 
 const app = (
     <Provider store={store}>
