fc5c206ccae65cea6a820181562525bace9a6724
Timur Kim <kimtimur01@gmail.com>
2022-10-04 13:57:27 +0600

    hw89 Changed fixtures, added "published" field with different values. And added roles for users. (Api)

--- api/fixtures.js
+++ api/fixtures.js
@@ -21,20 +21,24 @@ const run = async () => {
         username: "admin",
         password: "admin",
         token: uniqid(),
+        role: "admin",
     }, {
         username: "user",
         password: "user",
         token: uniqid(),
+        role: "user",
     });
 
     const [artist1, artist2] = await Artist.create({
         name: "Timur",
         information: "Timur is artist",
         image: "fixtures/dog.jpg",
+        published: true,
     }, {
         name: "Serkhan",
         information: "Serkhan is artist",
         image: "fixtures/sad.jpg",
+        published: true,
     });
 
     const [albumTimur, albumSerkhan1, albumSerkhan2] = await Album.create({
@@ -42,43 +46,56 @@ const run = async () => {
         artist: artist1['_id'],
         release: "2022-09-28T09:43:14.374Z",
         image: "fixtures/dog.jpg",
+        published: true,
     }, {
         title: "Tracks 1 by Serkhan",
         artist: artist2['_id'],
         release: "2022-07-28T09:43:14.374Z",
+        published: true,
     }, {
         title: "Tracks 2 by Serkhan",
         artist: artist2['_id'],
         release: "2022-08-28T09:43:14.374Z",
+        published: false,
     });
 
-    const [trackTimur1, trackTimur2, trackSerkhan1, trackSerkhan2, trackSerkhan3] = await Track.create({
+    const [trackTimur1, trackTimur2, trackSerkhan1, trackSerkhan2] = await Track.create({
         title: "Track Timur 1",
+        artist: artist1['_id'],
         album: albumTimur['_id'],
         duration: "2:12",
         number: 1,
         youtube: "hUTItsO2dfw",
+        published: true,
     }, {
         title: "Track Timur 2",
+        artist: artist1['_id'],
         album: albumTimur['_id'],
         duration: "3:25",
         number: 2,
+        published: true,
     }, {
         title: "Track Serkhan 1",
+        artist: artist2['_id'],
         album: albumSerkhan1['_id'],
         duration: "1:45",
         number: 1,
         youtube: "f2fupOTYgUI",
+        published: true,
     }, {
         title: "Track Serkhan 2",
+        artist: artist2['_id'],
         album: albumSerkhan1['_id'],
         duration: "4:15",
         number: 2,
+        published: true,
     }, {
         title: "Track Serkhan 1",
+        artist: artist2['_id'],
         album: albumSerkhan2['_id'],
         duration: "2:21",
         number: 1,
+        published: false,
     });
 
     await TrackHistory.create({
@@ -105,10 +122,6 @@ const run = async () => {
         user: user2['_id'],
         track: trackTimur2,
         datetime: "2022-09-28T09:54:00.355Z",
-    }, {
-        user: user2['_id'],
-        track: trackSerkhan3,
-        datetime: "2022-09-28T09:41:00.355Z",
     });
 
     await mongoose.connection.close();
