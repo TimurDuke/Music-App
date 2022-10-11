66b59e8b8fdab35369088ab40d166ac308dc9ad6
Timur Kim <kimtimur01@gmail.com>
2022-10-11 15:44:53 +0600

    hw91 Added email, displayName, avatarImage, facebookId fields to User model. (Api)

--- api/models/User.js
+++ api/models/User.js
@@ -2,23 +2,35 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const uniqid = require("uniqid");
 
-const { Schema, model } = mongoose;
+const {Schema, model} = mongoose;
 
 const SALT_WORK_FACTOR = 10;
+
+const usernameValidateUnique = async value => {
+    const user = await User.findOne({username: value});
+
+    if (user) return false;
+};
+
+const emailValidateUnique = async value => {
+    const user = await User.findOne({email: value});
+
+    if (user) {
+        return false;
+    }
+};
+
+const emailPatternValidate = value => {
+    return (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        .test(value));
+};
 
 const UserSchema = new Schema({
     username: {
         type: String,
         required: true,
         unique: true,
-        validate: {
-            validator: async value => {
-                const user = await User.findOne({username: value});
-
-                if (user) return false;
-            },
-            message: 'This user is already registered',
-        }
+        validate: {validator: usernameValidateUnique, message: 'This user is already registered'}
     },
     password: {
         type: String,
@@ -33,7 +45,24 @@ const UserSchema = new Schema({
         required: true,
         default: 'user',
         enum: ['admin', 'user'],
-    }
+    },
+    email: {
+        type: String,
+        required: true,
+        validate: [
+            {validator: emailPatternValidate, message: 'Invalid mail format'},
+            {validator: emailValidateUnique, message: 'This user is already registered'},
+        ]
+    },
+    displayName: {
+        type: String,
+        required: true,
+    },
+    avatarImage: {
+        type: String,
+        required: true,
+    },
+    facebookId: String,
 });
 
 UserSchema.pre('save', async function (next) {
